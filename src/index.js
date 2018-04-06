08a2674aa465e382cb3d59c57b4f7b5c0a417072
quentin <quentin.vernot@tanker.io>
2018-04-06 11:05:43 +0200

    don't use String.prototype.startsWith

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@
 export type SpecialReducer = (state: Object, payload: Object) => Object;
 export type SpecialReducers = { [string]: SpecialReducer };
 
+function startsWith(haystack: string, needle: string) {
+  return haystack.substr(0, needle.length) === needle;
+}
+
 function makeDumbReducer(prefix: string, initialState: Object = {}, specialReducers: SpecialReducers = {}) {
   return function(state: Object = initialState, { type, ...payload }: { type: string } = {}) {
     if (type) {
       if (specialReducers[type]) {
         return specialReducers[type](state, payload);
-      } else if (type.startsWith(prefix)) {
+      } else if (startsWith(type, prefix)) {
         return {
           ...state,
           ...payload,
